8c95a6b107fd6692be38a785d1d0065cccd54a1f
Deepakraja03 <12306deepakraja@gmail.com>
2024-02-17 15:16:04 +0530

    removed button

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -54,7 +54,6 @@ const Home = () => {
         </div>
       </div>
       <div className="flex justify-center">
-      <button className="border-2 bg-green-600 font-semibold rounded-xl p-2" onClick={handleUpload}>Upload Image</button>
       </div>
       </div>
       
