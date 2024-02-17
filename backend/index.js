40d681cdf302cec496e4d8751c3950398d51d2be
Deepakraja03 <12306deepakraja@gmail.com>
2024-02-17 07:39:36 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors('https://imageviewer-flame.vercel.app'));
 dbURI = process.env.MONGOURI;
 
 //Connect mongodb
